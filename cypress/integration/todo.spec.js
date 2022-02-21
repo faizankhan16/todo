a2ddd303621575c4bebec941118cceedae12dfe8
taimuraamer <taimur.aamer@gmail.com>
2022-02-21 08:05:25 +0500

    poiuy

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -7,7 +7,15 @@ describe('First test', () => {
     it('verify todo text', () => {
         cy.contains('Things To Do');
     });
-    it('verify todo text 1', () => {
-        cy.contains('Things To Do');
+    it('checking the checkboxes', () => {
+        cy.get('input[type="checkbox"]').eq(0).check();
+        cy.get('input[type="checkbox"]').eq(1).check();
+        cy.get('input[type="checkbox"]').eq(2).check();
     });
+    it('verify if the checkboxes have been checked', () => {
+        cy.get('input[type="checkbox"]').eq(0).should('be.checked');
+        cy.get('input[type="checkbox"]').eq(1).should('be.checked');
+        cy.get('input[type="checkbox"]').eq(2).should('be.checked');
+    });
+        
 });
